7ea9127783db810e47b6b67d1c952955e329bf02
Victor Tovar <victog21@outlook.com>
2021-06-09 15:47:40 -0500

    Se añadio la estructura de las tablas faltantes y se elimaro los factories creados

---
+++ database/migrations/2021_06_09_141745_create_numbers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNumbersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('numbers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('saving_id')->constrained()->onDelete('cascade');
+            $table->date('date_payment');
+            $table->intenger('shared_numbers')->default(0);
+            $table->boolean('pay_out')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('numbers');
+    }
+}
